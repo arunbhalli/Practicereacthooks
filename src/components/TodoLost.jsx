83b183d7891b09f28bc6451968054fff1bbbc52e
arunbhalli <tomarbhalli@gmail.com>
2021-05-29 10:55:03 +0200

    Add function for user to add new todo in list

--- src/components/TodoLost.jsx
+++ src/components/TodoLost.jsx
@@ -1,27 +1,22 @@
-import React,{useState} from 'react';
-
+import React, { useState } from 'react';
+import AddNewTodo from './AddNewTodo';
 const TodoList = () => {
- 
- const [todos, setTodos] =useState([
-    {text: 'Pay bills', id: 1},
-    {text: 'Do your Homework', id: 2},
-    {text: 'Feed the dogs', id: 3},
-  ])
-  const addTodo =() => {
-    setTodos([
-      ...todos, {text: 'Learn Hooks', id: Math.random()}
-    ]);
-  }
+  const [todos, setTodos] = useState([
+    { text: 'Pay bills', id: 1 },
+    { text: 'Do your Homework', id: 2 },
+    { text: 'Feed the dogs', id: 3 },
+  ]);
+  const addTodo = (text) => {
+    setTodos([...todos, { text, id: Math.random() }]);
+  };
   return (
     <div>
       <ul>
-      {todos.map((todo) => {
-        return(<li key={todo.id}>
-          {todo.text}
-          </li>)
-      })}
+        {todos.map((todo) => {
+          return (<li key={todo.id}>{todo.text}</li>);
+        })}
       </ul>
-      <button onClick={addTodo}> Add a Todo</button>
+      <AddNewTodo addTodo={addTodo} />
     </div>
   );
 };
